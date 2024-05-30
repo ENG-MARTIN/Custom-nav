0b7aa97d0eb8d37c695c68e649e3c4e2a47707ac
chipcode <chipcode@chipcode.www.chipcode.com>
2024-05-30 16:55:24 -0400

    ...

---
+++ navscript.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggle-button');
+    const navbarLinks = document.getElementById('navbar-links');
+
+    toggleButton.addEventListener('click', () => {
+        navbarLinks.classList.toggle('active');
+    });
+});
